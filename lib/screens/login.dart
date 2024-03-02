ead7b2790d9418199e56492ca270a21c897d2684
Chloe Gao <58396550+chgaobt@users.noreply.github.com>
2024-03-02 16:33:05 -0500

    added to login screen

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -14,7 +14,35 @@ class login extends StatelessWidget {
               height: MediaQuery.of(context).size.height,
               width: MediaQuery.of(context).size.width,
               child: Image.asset("images/tami_loginScreen.png",fit: BoxFit.fill),
-            )
+            ),
+            Positioned(
+              top: 100,
+              left: 170,
+              child: Text(
+                "Login",
+                style: TextStyle(
+                  fontSize: 35, 
+                  fontWeight: FontWeight.bold, 
+                  color: Colors.white),
+              )
+            ),
+            Positioned(
+              child: Column(
+                children: [
+                  Container(
+                    width: 300,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(Icons.person_outlined),
+                      ],
+                    ),
+                  )
+                ],
+              ))
           ],
         )],
       ),
