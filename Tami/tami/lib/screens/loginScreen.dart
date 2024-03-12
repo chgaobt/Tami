ed38d31bc077d30f4692085900374ade7d6d10ad
Chloe Gao <58396550+chgaobt@users.noreply.github.com>
2024-03-12 00:08:16 -0400

    controller constant value error:

--- Tami/tami/lib/screens/loginScreen.dart
+++ Tami/tami/lib/screens/loginScreen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -17,142 +19,125 @@ class _LoginState extends State<Login> {
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: Column(
+          child: SingleChildScrollView(
+              child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Stack(
-                children: [
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height,
-                    width: MediaQuery.of(context).size.width,
-                    child: Image.asset(
-                      'images/loginScreen.png',
-                      fit: BoxFit.fill,
-                    ),
-                  ),
-                  const Positioned(
-                    top: 200,
-                    left: 170,
-                    child: Text(
-                      "Tami",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontSize: 35,
-                          fontWeight: FontWeight.bold,
-                          color: Color.fromARGB(255, 213, 152, 113)),
-                    ),
-                  ),
-                  Positioned(
-                      top: 275,
-                      left: 45,
-                      child: SingleChildScrollView(
-                          child: Column(
-                        children: [
-                          Container(
-                            width: 350,
-                            decoration: BoxDecoration(
-                                //color: const Color.fromARGB(150, 213,152,113),
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(25)),
-                            child: const Row(
-                              children: [
-                                Padding(
-                                  padding: EdgeInsets.only(left: 25),
-                                  child: Icon(Icons.person_outlined,
-                                      color: Colors.black),
-                                ),
-                                Expanded(
-                                  child: Padding(
-                                    padding: EdgeInsets.only(
-                                      left: 18,
-                                    ),
-                                    child: TextField(
-                                      controller: _usernameController,
-                                      decoration: InputDecoration(
-                                          hintText: "Enter Username",
-                                          hintStyle:
-                                              TextStyle(color: Colors.black45),
-                                          border: InputBorder.none),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          //const SizedBox(height: 15),
-                          passwordField(),
-                          const SizedBox(height: 40),
-                          Container(
-                              alignment: Alignment.center,
-                              height: 50,
-                              width: 300,
-                              decoration: BoxDecoration(
-                                  color: const Color(0xFFD59871),
-                                  borderRadius: BorderRadius.circular(25)),
-                              child: const Padding(
-                                padding: EdgeInsets.symmetric(horizontal: 25.0),
-                                child: Center(
-                                  child: Text(
-                                    'Login',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.white),
-                                  ),
-                                ),
-                              )),
-                          const SizedBox(height: 15),
-                          const Text(
-                            'Forgot your password?',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontSize: 15,
-                                //fontWeight: FontWeight.bold,
-                                color: Colors.black45),
-                          ),
-                          const SizedBox(height: 190),
-                          const Text(
-                            "or Login with",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                //color: Color.fromARGB(255,213,152,113)
-                                color: Colors.black54),
-                          ),
-                          Row(children: [
-                            Container(
-                              height: 45,
-                              width: 177,
-                              decoration: BoxDecoration(
-                                  color: Colors.lightBlue,
-                                  borderRadius: BorderRadius.circular(50)),
-                            ),
-                            Container(
-                              height: 45,
-                              width: 177,
-                              decoration: BoxDecoration(
-                                  color: Colors.lightBlue,
-                                  borderRadius: BorderRadius.circular(50)),
-                            ),
-                          ]),
-                          const SizedBox(height: 20),
-                          const Text(
-                            "Don't have an account? Sign up",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold,
-                                //color: Color.fromARGB(255,213,152,113)
-                                color: Colors.black54),
-                          ),
-                        ],
-                      )))
-                ],
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Image.asset(
+                  'images/loginScreen.png',
+                  fit: BoxFit.fill,
+                ),
               ),
+              Text(
+                "Tami",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 213, 152, 113)),
+              ),
+              Container(
+                width: 350,
+                decoration: BoxDecoration(
+                  //color: const Color.fromARGB(150, 213,152,113),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: const Row(
+                    children: [
+                      Icon(
+                        Icons.person_outlined,
+                        color: Colors.black,
+                      ),
+
+
+                      TextField(
+                                  
+                                  decoration: InputDecoration(
+                                      hintText: "Enter Username",
+                                      hintStyle:
+                                          TextStyle(color: Colors.black45),
+                                      border: InputBorder.none),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+              ),
+        
+                      const SizedBox(height: 40),
+                      Container(
+                          alignment: Alignment.center,
+                          height: 50,
+                          width: 300,
+                          decoration: BoxDecoration(
+                              color: const Color(0xFFD59871),
+                              borderRadius: BorderRadius.circular(25)),
+                          child: const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 25.0),
+                            child: Center(
+                              child: Text(
+                                'Login',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                            ),
+                          )),
+                      const SizedBox(height: 15),
+                      const Text(
+                        'Forgot your password?',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 15,
+                            //fontWeight: FontWeight.bold,
+                            color: Colors.black45),
+                      ),
+                      const SizedBox(height: 190),
+                      const Text(
+                        "or Login with",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            //color: Color.fromARGB(255,213,152,113)
+                            color: Colors.black54),
+                      ),
+                      Row(children: [
+                        Container(
+                          height: 45,
+                          width: 177,
+                          decoration: BoxDecoration(
+                              color: Colors.lightBlue,
+                              borderRadius: BorderRadius.circular(50)),
+                        ),
+                        Container(
+                          height: 45,
+                          width: 177,
+                          decoration: BoxDecoration(
+                              color: Colors.lightBlue,
+                              borderRadius: BorderRadius.circular(50)),
+                        ),
+                      ]),
+                      const SizedBox(height: 20),
+                      const Text(
+                        "Don't have an account? Sign up",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            //color: Color.fromARGB(255,213,152,113)
+                            color: Colors.black54),
+                      ),
+                    ],
+                  ))
             ],
-          ),
+          )),
         ),
       ),
     );
