1ec4ba48607d0f0a6fdb1acbfa87a84836c07001
Chloe Gao <58396550+chgaobt@users.noreply.github.com>
2024-06-22 17:01:40 -0400

    fixing firebase env requirements

--- Tami/tami/lib/main.dart
+++ Tami/tami/lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:tami/screens/splash.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
